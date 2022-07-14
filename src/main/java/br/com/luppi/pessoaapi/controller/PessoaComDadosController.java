d1200cdb6dbd1375dd2a36a54a2c785f6561534c
Gabriel Luppi <luppi.gabriel08@gmail.com>
2022-07-14 11:01:03 -0300

    Adicionando documentacao PessoaComDados

--- src/main/java/br/com/luppi/pessoaapi/controller/PessoaComDadosController.java
+++ src/main/java/br/com/luppi/pessoaapi/controller/PessoaComDadosController.java
@@ -1,9 +1,11 @@
 package br.com.luppi.pessoaapi.controller;
 
+import br.com.luppi.pessoaapi.documentation.PessoaComDadosDocumentation;
 import br.com.luppi.pessoaapi.dto.PessoaComDadosDTO;
 import br.com.luppi.pessoaapi.exception.EntidadeNaoEncontradaException;
 import br.com.luppi.pessoaapi.service.PessoaComDadosService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,19 +15,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/pessoa-com-dados")
 @Validated
-public class PessoaComDadosController {
+public class PessoaComDadosController implements PessoaComDadosDocumentation {
 
     @Autowired
     private PessoaComDadosService pessoaComDadosService;
 
     @PostMapping
-    public PessoaComDadosDTO post(@Valid @RequestBody PessoaComDadosDTO pessoaComDadosDTO) {
-        return pessoaComDadosService.create(pessoaComDadosDTO);
+    public ResponseEntity<PessoaComDadosDTO>  post(@Valid @RequestBody PessoaComDadosDTO pessoaComDadosDTO) {
+        return ResponseEntity.ok(pessoaComDadosService.create(pessoaComDadosDTO));
     }
 
     @GetMapping
-    public List<PessoaComDadosDTO> get() {
-        return pessoaComDadosService.list();
+    public ResponseEntity<List<PessoaComDadosDTO>>  get() {
+        return ResponseEntity.ok(pessoaComDadosService.list());
     }
 
     @DeleteMapping("/{cpf}")
@@ -34,8 +36,8 @@ public class PessoaComDadosController {
     }
 
     @PutMapping("/{cpf}")
-    public PessoaComDadosDTO put(@PathVariable("cpf") String cpf, @RequestBody PessoaComDadosDTO pessoaComDadosDTO) throws EntidadeNaoEncontradaException {
-        return pessoaComDadosService.update(cpf, pessoaComDadosDTO);
+    public ResponseEntity<PessoaComDadosDTO>  put(@PathVariable("cpf") String cpf, @RequestBody PessoaComDadosDTO pessoaComDadosDTO) throws EntidadeNaoEncontradaException {
+        return ResponseEntity.ok(pessoaComDadosService.update(cpf, pessoaComDadosDTO));
     }
 
 }
